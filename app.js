2855ba0700b7adde4e4412b43107b332b0fbda9a
sr0876392 <42741498+sr0876392@users.noreply.github.com>
2018-08-29 08:44:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+//Single line comm
+/*multiline
+comment*/
 console.log("Hello JS");
+// value or data types
+console.log("Hello from JS");
+console.log("Let's do some math");
+console.log(4+7);
+console.log("12/8");
+console.log("Bye!");
